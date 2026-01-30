d03061ae997f0cc6cab8471e007769b7eee259c4
webwizzz <verma.s@somaiya.edu>
2026-01-30 20:17:56 +0530

    init

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Meenakshi.H\OneDrive\Documents\MCP\digital_twin\.venv
+set VIRTUAL_ENV=C:\Users\swati\OneDrive\Desktop\digital_twin\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
